70746daa99ebce0b5124079d62fe002c7527cc55
HakimaNoori <hakima.noori.1379@gmail.com>
2025-02-11 09:05:12 +0430

    Add code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import MainVid from "../assets/video/main.mp4";
 import BannerImg from "../components/BannerImg/BannerImg";
 import Hero from "../components/Hero/Hero";
 import Places from "../components/Places/Places";
-import Poster from "../assets/cover-women.jpg"; 
+import Poster from "../assets/cover-women.jpg";
 import Blogs from "./Blogs";
 import Banner from "../components/Banner/Banner";
 import Banner2 from "../assets/travel-cover2.jpg";
@@ -10,24 +10,26 @@ import Testimonial from "../components/Testimonial/Testimonial";
 const Home = () => {
   return (
     <>
-      <div className="relative h-[700px]">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
-        >
-          <source src={MainVid} type="video/mp4" />
-        </video>
-        <Hero />
+      <div>
+        <div className="relative h-[700px]">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+          >
+            <source src={MainVid} type="video/mp4" />
+          </video>
+          <Hero />
+        </div>
+        <Places />
+        <BannerImg img={Poster} />
+        <Blogs />
+        <Banner />
+        <BannerImg img={Banner2} />
+        <Testimonial />
       </div>
-      <Places />
-      <BannerImg img={Poster} />
-      <Blogs />
-      <Banner />
-      <BannerImg img={Banner2} />
-      <Testimonial/>
     </>
   );
 };
